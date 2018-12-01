15d400ffacffd6fd07aa3d1a4b9f5617ce3a1cda
Paul Cowan <paul.cowan@bjss.com>
2018-12-01 13:50:13 +0000

    first commit

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import           Data.List.Split
+
+parseString :: Integer -> String -> Integer
+parseString acc s =
+  let num = read $ tail s :: Integer
+      operator = head s
+  in if operator == '+'
+       then acc + num
+       else acc - num
+
+sumInput = foldl parseString 0
+
+main :: IO ()
+main = do
+  file <- readFile "input.txt"
+  let input = filter (not . null) $ splitOn "\n" file
+  print (sumInput input)
