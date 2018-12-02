44990b91602aaf43bd5261ca4c13b15974004fcd
Paul Cowan <paul.cowan@bjss.com>
2018-12-02 15:09:02 +0000

    day2 part 1

--- src/Day2Part1.hs
+++ src/Day2Part1.hs
@@ -4,22 +4,32 @@ import           Data.List (group, intercalate, nub, sort)
 
 flatten = intercalate []
 
-getCheckSum :: [String] -> Int
+getOccurrences :: (Integer, Integer) -> [Int] -> (Integer, Integer)
+getOccurrences acc xs =
+  let twos =
+        if 2 `elem` xs
+          then 1
+          else 0
+      threes =
+        if 3 `elem` xs
+          then 1
+          else 0
+  in (fst acc + twos, snd acc + threes)
+
 getCheckSum boxes =
   let grouped =
-        flatten $
-        map
-          (nub .
-           sort .
-           map length .
-           filter (\x -> length x == 2 || length x == 3) . group . sort)
-          boxes
-  in let twos = length $ filter (== 2) grouped
-         threes = length $ filter (== 3) grouped
-     in twos
+        map nub $
+        map (\x -> map length x) $
+        map (filter (\x -> length x == 2 || length x == 3) . group . sort) boxes
+      {- in let twos = length $ filter (== 2) grouped
+         threes = length $ filter (== 3) grouped -}
+  in foldl getOccurrences (0, 0) grouped
 
 main :: IO ()
-main = do
-  let file = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n"
+main
+{- let file = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n" -}
+{- file <- readFile "day2.txt" -}
+ = do
+  file <- readFile "day2.txt" -- "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n"
   let input = lines file
   print (getCheckSum input)
