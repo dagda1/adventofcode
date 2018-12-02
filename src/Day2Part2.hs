71e473dacb19eadebd6de09e086291befda69a2a
Paul Cowan <paul.cowan@bjss.com>
2018-12-02 21:25:25 +0000

    day 2 part 2

--- src/Day2Part2.hs
+++ src/Day2Part2.hs
@@ -1,9 +1,26 @@
 module Main where
 
-import           Data.List (group, intercalate, nub, sort)
+import           Data.List (intersect, sort)
+
+diffString :: String -> String -> (Integer, Int)
+diffString x y =
+  foldl
+    (\(a, b) z ->
+        if x !! b == y !! b
+          then (a, b + 1)
+          else (a + 1, b + 1))
+    (0, 0)
+    x
+
+findIt :: [String] -> String
+findIt xs =
+  let [[left], [right]] =
+        filter (not . null) $
+        map (\x -> filter (\y -> fst (diffString x y) == 1) xs) xs
+  in intersect left right
 
 main :: IO ()
 main = do
-  file <- "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n"
+  file <- readFile "day2.txt"
   let input = lines file
-  print (getCheckSum input)
+  print (findIt input)
